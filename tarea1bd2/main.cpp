cb3adab975be4943ae5cf3faea2781b42087b367
Luis-ntonio <63657624+Luis-ntonio@users.noreply.github.com>
2022-04-01 23:22:39 -0500

    Update main.cpp

--- tarea1bd2/main.cpp
+++ tarea1bd2/main.cpp
@@ -1,5 +1,55 @@
 #include <iostream>
-#include "p4.cpp"
+#include "p1.cpp" //cambiar para acceder a otra pregunta
 int main(){
     textoo();
 }
+
+/*
+ejemplos para copiar y pegar en consola
+archivo.dat | archivo.txt
+
+
+
+
+Howard|Paredes Zegarra|Computacion|1500.00 
+Penny|Vargas Cordero|Industrial|2550.50
+Sheldon|Cooper Quizpe|Mecatronica|1850.00
+Lidya|Cooper Quizpe|Mecatronica|1850.00
+Thony|Cooper Quizpe|Mecatronica|1850.00
+
+
+0001
+Howard
+Paredes
+Zegarra
+Computacion
+0002
+Penny
+Vargas
+Cordero
+Industrial     
+0003
+Sheldon
+Cooper
+Vega
+Mecatronica
+0004
+Pamela
+Vargas
+Cordero
+Industrial
+0005
+Vanesa
+Cooper
+Quizpe
+Mecatronica
+0006
+Jesus
+Vega
+Cordero
+Industrial
+0007
+Heider
+Sanchez
+Quizpe
+Mecatronica
